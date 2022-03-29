cc51942b46b8c04bb3510a45b01b8546d4628be9
Gokhan Gulbiz <ggulbiz@gmail.com>
2022-03-29 18:06:29 +0300

    Boiling added to predefined weathers.

--- GithubActions.Demo/GithubActions.Demo.API/Controllers/WeatherForecastController.cs
+++ GithubActions.Demo/GithubActions.Demo.API/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace GithubActions.Demo.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Boiling"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
