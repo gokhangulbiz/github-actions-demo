aa8b7945c7f3d56c3b174d02697463835f9ef2e1
Gokhan Gulbiz <ggulbiz@gmail.com>
2022-03-29 13:00:38 +0300

    Initial commit

---
+++ GithubActions.Demo/GithubActions.Demo.API.UnitTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace GithubActions.Demo.API.UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        public void Test2()
+        {
+            Assert.Pass();
+        }
+
+        public void Test3()
+        {
+            Assert.Pass();
+        }
+
+        public void Test4()
+        {
+            Assert.Pass();
+        }
+
+        public void Test5()
+        {
+            Assert.Pass();
+        }
+    }
+}
